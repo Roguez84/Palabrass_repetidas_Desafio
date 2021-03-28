972135628e42c78b7532fced6432dcf7cebdf64c
Roguez84 <t.roguez984@gmail.com>
2021-03-28 19:26:26 -0300

    codigo funciona pero faltaria ver como se arreglan palabras separadas por puntos y comas

--- codigo.js
+++ codigo.js
@@ -2,10 +2,12 @@ var p1 = document.querySelector("#texto-entrada");
 //console.log(typeof(p1.textContent));
 var texto = p1.textContent;
 
+
+// Parte 1: Contando letras
 var contador_letras = {
 };
 
-for(var i = 1; i < 12; i++) {
+for(var i = 0; i < texto.length; i++) {
   console.log(texto.charAt(i));
 
   if (!contador_letras[texto.charAt(i)]) {
@@ -16,10 +18,41 @@ for(var i = 1; i < 12; i++) {
   }
 }
 
-console.log(contador_letras);
+//console.log(contador_letras);
 
 // codigo para iterar object viene de https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
 for (const [key, value] of Object.entries(contador_letras)) {
+  //console.log(`${key}: ${value}`);
+  var nuevo_tag = document.createElement("p");
+  var texto_del_tag = document.createTextNode(`${key}: ${value}`);
+  nuevo_tag.appendChild(texto_del_tag);
+  document.getElementById("resultados").appendChild(nuevo_tag);
+}
+
+
+// Parte 2: Contando palabras
+var texto_limpio_de_puntos = texto.replaceAll(".", "");
+var texto_limpio_de_puntos_y_comas = texto_limpio_de_puntos.replaceAll(",", "");
+var array_palabras = texto_limpio_de_puntos_y_comas.split(" ");
+//console.log(array_palabras);
+var contador_palabras = {
+};
+
+
+for(var i = 0; i < array_palabras.length; i++) {
+  //console.log(texto.charAt(i));
+
+  if (!contador_palabras[array_palabras[i]]) {
+  contador_palabras[array_palabras[i]] = 1;
+  }
+  else {
+    contador_palabras[array_palabras[i]] += 1;
+  }
+}
+//console.log(contador_palabras);
+
+
+for (const [key, value] of Object.entries(contador_palabras)) {
   //console.log(`${key}: ${value}`);
   var nuevo_tag = document.createElement("p");
   var texto_del_tag = document.createTextNode(`${key}: ${value}`);
